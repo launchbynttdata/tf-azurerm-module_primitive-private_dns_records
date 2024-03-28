d58a3ea0af1a5c538c9a23dec1cc8b99dcfe1a5f
Ben Vaughan <ben.vaughan@nttdata.com>
2024-03-28 15:17:19 -0500

    'Migration validation fixes for make check'

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -2,14 +2,15 @@ package common
 
 import (
 	"context"
+	"os"
 	"strings"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/profiles/latest/privatedns/mgmt/privatedns"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/privatedns/armprivatedns"
+
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/launchbynttdata/lcaf-component-terratest/lib/azure/configure"
-	"github.com/launchbynttdata/lcaf-component-terratest/lib/azure/dns"
-	"github.com/launchbynttdata/lcaf-component-terratest/lib/azure/login"
 	"github.com/launchbynttdata/lcaf-component-terratest/types"
 	"github.com/stretchr/testify/assert"
 )
@@ -17,19 +18,27 @@ import (
 const terraformDir string = "../../examples/dns_zone_records"
 const varFile string = "test.tfvars"
 
-func TestDnsZoneRecords(t *testing.T, ctx types.TestContext) {
-	envVarMap := login.GetEnvironmentVariables()
-	clientID := envVarMap["clientID"]
-	clientSecret := envVarMap["clientSecret"]
-	tenantID := envVarMap["tenantID"]
-	subscriptionID := envVarMap["subscriptionID"]
+var (
+	privatednsClientFactory *armprivatedns.ClientFactory
+	dnsZoneRecordSetsClient *armprivatedns.RecordSetsClient
+)
 
-	spt, err := login.GetServicePrincipalToken(clientID, clientSecret, tenantID)
+func TestDnsZoneRecords(t *testing.T, ctx types.TestContext) {
+	subscriptionID := os.Getenv("ARM_SUBSCRIPTION_ID")
+	if subscriptionID == "" {
+		t.Fatalf("ARM_SUBSCRIPTION_ID must be set for acceptance tests")
+	}
+	credential, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
-		t.Fatalf("Error getting Service Principal Token: %v", err)
+		t.Fatalf("Error getting credentials: %e\n", err)
 	}
 
-	dnsZoneRecordSetsClient := dns.GetPrivateDNSZoneRecordSetsClient(spt, subscriptionID)
+	privatednsClientFactory, err = armprivatedns.NewClientFactory(subscriptionID, credential, nil)
+	if err != nil {
+		t.Fatalf("Error getting privatedns factory: %e\n", err)
+	}
+
+	dnsZoneRecordSetsClient = privatednsClientFactory.NewRecordSetsClient()
 
 	terraformOptions := configure.ConfigureTerraform(terraformDir, []string{terraformDir + "/" + varFile})
 
@@ -38,22 +47,23 @@ func TestDnsZoneRecords(t *testing.T, ctx types.TestContext) {
 	})
 }
 
-func checkDNSZoneRecordSets(t *testing.T, dnsZoneRecordSetsClient privatedns.RecordSetsClient, terraformOptions *terraform.Options) {
-	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "a_record_ids", "a_record_fqdns", privatedns.A)
-	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "cname_record_ids", "cname_record_fqdns", privatedns.CNAME)
-	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "txt_record_ids", "txt_record_fqdns", privatedns.TXT)
+func checkDNSZoneRecordSets(t *testing.T, dnsZoneRecordSetsClient *armprivatedns.RecordSetsClient, terraformOptions *terraform.Options) {
+	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "a_record_ids", "a_record_fqdns", armprivatedns.RecordTypeA)
+	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "cname_record_ids", "cname_record_fqdns", armprivatedns.RecordTypeCNAME)
+	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "txt_record_ids", "txt_record_fqdns", armprivatedns.RecordTypeTXT)
 }
 
-func checkRecord(t *testing.T, dnsZoneRecordSetsClient privatedns.RecordSetsClient, terraformOptions *terraform.Options, recordSetIdsKey string, recordSetFQDNsKey string, recordType privatedns.RecordType) {
+func checkRecord(t *testing.T, dnsZoneRecordSetsClient *armprivatedns.RecordSetsClient, terraformOptions *terraform.Options, recordSetIdsKey string, recordSetFQDNsKey string, recordType armprivatedns.RecordType) {
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 	zoneName := terraform.Output(t, terraformOptions, "private_dns_zone_name")
 	recordSetIds := terraform.OutputMap(t, terraformOptions, recordSetIdsKey)
 	recordSetFQDNs := terraform.OutputMap(t, terraformOptions, recordSetFQDNsKey)
+	options := armprivatedns.RecordSetsClientGetOptions{}
 
 	for key, recordSetId := range recordSetIds {
 		relativeRecordSetName := getSubdomain(recordSetFQDNs[key])
 
-		dnsZoneRecordSets, err := dnsZoneRecordSetsClient.Get(context.Background(), resourceGroupName, zoneName, recordType, relativeRecordSetName)
+		dnsZoneRecordSets, err := dnsZoneRecordSetsClient.Get(context.Background(), resourceGroupName, zoneName, recordType, relativeRecordSetName, &options)
 		if err != nil {
 			t.Fatalf("Error getting DNS Zone: %v", err)
 		}
@@ -61,7 +71,7 @@ func checkRecord(t *testing.T, dnsZoneRecordSetsClient privatedns.RecordSetsClie
 		assert.Equal(t, strings.ToLower(recordSetId), strings.ToLower(*dnsZoneRecordSets.ID))
 		assert.Equal(t, strings.ToLower(relativeRecordSetName), strings.ToLower(*dnsZoneRecordSets.Name))
 		assert.Equal(t, strings.ToLower((string(recordType))), getRecordType(strings.ToLower(*dnsZoneRecordSets.Type)))
-		assert.NotEmpty(t, dnsZoneRecordSets.RecordSetProperties)
+		assert.NotEmpty(t, dnsZoneRecordSets.Properties)
 	}
 }
 
